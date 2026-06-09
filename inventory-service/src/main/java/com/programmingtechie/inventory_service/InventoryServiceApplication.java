af2b795f431493bb2bfda41b38aee6d4c27b391d
NguyenTanNghi <nguyentannghi5722@gmail.com>
2026-06-09 14:53:07 +0700

    tạo inventory

--- inventory-service/src/main/java/com/programmingtechie/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/programmingtechie/inventory_service/InventoryServiceApplication.java
@@ -1,7 +1,12 @@
 package com.programmingtechie.inventory_service;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.programmingtechie.inventory_service.model.Inventory;
+import com.programmingtechie.inventory_service.repository.InventoryRepository;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -9,5 +14,19 @@ public class InventoryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
+    @Bean
+    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+        return args -> {
+            Inventory inventory = new Inventory();
+            inventory.setSkuCode("iphone-13");
+            inventory.setQuantity(100);
+            inventoryRepository.save(inventory);
+
+            Inventory inventory2 = new Inventory();
+            inventory2.setSkuCode("iphone-13-red");
+            inventory2.setQuantity(0);
+            inventoryRepository.save(inventory2);
+        };
+    }
 
 }
