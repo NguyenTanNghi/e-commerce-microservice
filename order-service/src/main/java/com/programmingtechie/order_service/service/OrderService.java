15b9f283fd9d199932fd97f20cbe0832cb3d6ad7
NguyenTanNghi <nguyentannghi5722@gmail.com>
2026-06-10 20:24:45 +0700

    cấu hình eureka cho các service

--- order-service/src/main/java/com/programmingtechie/order_service/service/OrderService.java
+++ order-service/src/main/java/com/programmingtechie/order_service/service/OrderService.java
@@ -22,7 +22,7 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClient;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -34,8 +34,8 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
 
         // call inventory service and place order if product is in stock
-        InventoryResponse[] inventoryResponsesArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponsesArray = webClient.build().get()
+                .uri("http://INVENTORY-SERVICE/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
