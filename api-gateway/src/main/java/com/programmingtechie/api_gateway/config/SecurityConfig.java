f84b94538b8511a49f82dfaf4b996b6a32b3a209
NguyenTanNghi <nguyentannghi5722@gmail.com>
2026-06-13 16:10:42 +0700

    xử lý trường hợp gửi lại order khi inventory chưa xử lý

--- api-gateway/src/main/java/com/programmingtechie/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/programmingtechie/api_gateway/config/SecurityConfig.java
@@ -6,16 +6,35 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+// @Configuration
+// @EnableWebFluxSecurity
+// public class SecurityConfig {
+//     @Bean
+//     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+//         serverHttpSecurity
+//                 .csrf().disable()
+//                 .authorizeExchange(exchanges -> exchanges
+//                         .pathMatchers("/eureka/**").permitAll().anyExchange().authenticated())
+//                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+//         return serverHttpSecurity.build();
+//     }
+// }
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
-        serverHttpSecurity
-                .csrf().disable()
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/eureka/**").permitAll().anyExchange().authenticated())
-                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
-        return serverHttpSecurity.build();
+                        .pathMatchers("/eureka/**").permitAll()
+                        .pathMatchers("/api/product/**").permitAll()
+                        .pathMatchers("/api/order/**").permitAll()
+                        .pathMatchers("/api/inventory/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt())
+                .build();
     }
 }
